0a0a3f365a0ec8ae7fd4839feb98c7e1dc77684e
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2021-05-17 17:20:02 +0100

    Added Cancel Button layout design in activity_login.xml, and its functionality in LoginActivity.kt

--- app/src/main/java/com/example/ecowas_facts/LoginActivity.kt
+++ app/src/main/java/com/example/ecowas_facts/LoginActivity.kt
@@ -19,6 +19,7 @@ class LoginActivity : AppCompatActivity() {
         val email = findViewById<TextInputEditText>(R.id.email_edit_text)
         val password = findViewById<TextInputEditText>(R.id.password_edit_text)
         val logInButton = findViewById<MaterialButton>(R.id.login_button)
+        val cancelButton = findViewById<MaterialButton>(R.id.cancel_button)
 
         logInButton.setOnClickListener {
             if (email.text.toString().isEmpty() || password.text.toString().isEmpty()){
@@ -27,11 +28,27 @@ class LoginActivity : AppCompatActivity() {
                 if (email.text.toString() == "user@email.com" && password.text.toString() == "09876"){
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
-                } else{
+                } else {
                     Toast.makeText(this,"Wrong Email or Password", Toast.LENGTH_LONG).show()
                 }
             }
         }
 
+        cancelButton.setOnClickListener {
+            if (email.text.toString().isEmpty() || password.text.toString().isEmpty()){
+                email.setText("")
+                password.setText("")
+
+                if (email.text.toString().isEmpty() && password.text.toString().isEmpty()){
+                    Toast.makeText(this,"No Login details provided", Toast.LENGTH_LONG).show()
+                }
+            } else if (email.text.toString().isNotEmpty() && password.text.toString().isNotEmpty()){
+                email.setText("")
+                password.setText("")
+            }
+
+            }
+        }
+
     }
-}
+
